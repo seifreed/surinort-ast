16f079672319fe87f34cd74851441016dd2a2189
Marc R. <mriverolopez@gmail.com>
2026-04-03 08:41:50 +0200

    chore(release): bump version to 2.0.1

--- src/surinort_ast/version.py
+++ src/surinort_ast/version.py
@@ -5,8 +5,8 @@ Licensed under GNU General Public License v3.0
 Author: Marc Rivero | @seifreed | mriverolopez@gmail.com
 """
 
-__version__ = "2.0.0"
-__ast_version__ = "2.0.0"
+__version__ = "2.0.1"
+__ast_version__ = "2.0.1"
 
 __author__ = "Marc Rivero López"
 __email__ = "mriverolopez@gmail.com"
